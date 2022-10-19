1525ae772460388dcc2ec9d1ea2591a71ead60ca
Keiran Paster <keiran@me.com>
2022-10-18 20:29:00 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,5 @@
 from setuptools import setup
 
-
-try:
-    # for pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError:
-    # for pip <= 9.0.3
-    from pip.req import parse_requirements
-
-
-def load_requirements(fname):
-    reqs = parse_requirements(fname, session="test")
-    return [str(ir.req) for ir in reqs]
-
-
 setup(
     name='stochastic_offline_envs',
     version='1.0',
@@ -21,5 +7,4 @@ setup(
     author='Keiran Paster',
     author_email='keirp@cs.toronto.edu',
     packages=['stochastic_offline_envs'],
-    install_requires=load_requirements('requirements.txt'),
 )
