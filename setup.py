42818dd76feb4bd40ee3f7e5b9034988d0d82567
Keiran Paster <keiran@me.com>
2022-10-18 20:24:33 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
 from setuptools import setup
 
 
+try:
+    # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError:
+    # for pip <= 9.0.3
+    from pip.req import parse_requirements
+
+
 def load_requirements(fname):
     reqs = parse_requirements(fname, session="test")
     return [str(ir.req) for ir in reqs]
